af163ca56d2e264bec04b55265a7baaededed5c9
Kent Green <King-of-Spades@users.noreply.github.com>
2020-03-24 10:46:41 -0700

    Update appcenter-post-clone.sh

--- Droid/appcenter-post-clone.sh
+++ Droid/appcenter-post-clone.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
 # Comments
-
-echo "Hello World! I'm a post-clone script!"
-
-echo "I run immediately after your project has finished cloning."
-
-echo "Documentation: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/#post-clone"
-
-echo "end post-clone script"
+echo "app center version info test"
+eval cat $HOME/systeminfo.md

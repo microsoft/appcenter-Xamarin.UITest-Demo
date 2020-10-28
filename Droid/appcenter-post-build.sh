dcbf59efb38ea3e7eefb23ec2897194ddd24f993
Kent Green <King-of-Spades@users.noreply.github.com>
2020-10-28 11:48:07 -0700

    Update appcenter-post-build.sh

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -29,8 +29,11 @@ eval MSBuild $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH -v:q
 # echo "contents of UITest directory:" 
 # ls $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH
 
+# Path to APK
+APK_PATH= $APPCENTER_OUTPUT_DIRECTORY/com.appcenter.UITestDemo.apk
+
 # Upload tests
-App_Center_Test_Command='appcenter test run uitest --app $TEAM_APP --devices $DEVICE_SET --app-path $APPCENTER_OUTPUT_DIRECTORY/com.appcenter.UITestDemo.apk  --test-series "gh-$APPCENTER_BRANCH" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH/bin/Debug --async --token $API_KEY --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools'
+App_Center_Test_Command='appcenter test run uitest --app $TEAM_APP --devices $DEVICE_SET --app-path $APK_PATH  --test-series "gh-$APPCENTER_BRANCH" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH/bin/Debug --async --token $API_KEY --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools'
 
 echo $App_Center_Test_Command
 eval $App_Center_Test_Command

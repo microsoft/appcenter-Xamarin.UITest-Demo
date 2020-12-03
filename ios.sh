d80a5ad6f19234b9fd4ee38fa38801f2b4d337ee
Kent Green <King-of-Spades@users.noreply.github.com>
2020-12-03 09:06:52 -0800

    Update ios.sh

--- ios.sh
+++ ios.sh
@@ -5,6 +5,9 @@
 # Step 1 Generate an AppCenter upload command and paste it to the variable
 AppCenter_Test_Command='paste upload command here'
 
+# Debugging upload command 
+# AppCenter_Test_Command='appcenter test run uitest --app "XTCTeam/Kent-G.-UITestDemo-1" --devices "XTCTeam/12-dot-4-6-13-dot-7" --app-path pathToFile.ipa --test-series "main" --locale "en_US" --build-dir pathToUITestBuildDir'
+
 # Step 2 Provide the (absolute or relative) path to the IPA
 app_path='precompiledApps/UITestDemo.ipa'
 

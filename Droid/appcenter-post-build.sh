70f563f077649659fc37aebef6cd059ea6d6e60f
Kent Green <King-of-Spades@users.noreply.github.com>
2021-01-16 09:42:31 -0800

    Update appcenter-post-build.sh

--- Droid/appcenter-post-build.sh
+++ Droid/appcenter-post-build.sh
@@ -16,8 +16,8 @@ echo "Start Test upload script (ac-test-run.sh)"
 sh ../ac-test-run.sh
 echo "Finish Test upload script (ac-test-run.sh)"
 
-#echo "Start Distribute script (ac-distribute.sh)"
-#sh ../ac-distribute.sh
-#echo "Finish Distribute script (ac-distribute.sh)"
+echo "Start Distribute script (ac-distribute.sh)"
+sh ../ac-distribute.sh
+echo "Finish Distribute script (ac-distribute.sh)"
 
 echo "end post-build script"
